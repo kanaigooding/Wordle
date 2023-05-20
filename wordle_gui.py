6c3b2d57959e0f634783ee1e3cf2a85cb767483e
KanaiGooding <kgooding@hawaii.edu>
2023-05-20 00:07:16 -1000

    1

--- wordle_gui.py
+++ wordle_gui.py
@@ -3,51 +3,70 @@
 import tkinter as tk
 from wordle_solver import WordleSolver
 
+
 class WordleGUI(tk.Tk):
     def __init__(self, solver):
         super().__init__()
+        self.configure(bg='light grey')
         self.title('Wordle Solver')
         self.geometry('400x300')
         self.solver = solver
         self.guesses = []
-        self.guess_entry_fields = []
-        self.guess_feedback_fields = []
-        self.feedback_colors = {'G': 'green', 'Y': 'yellow', 'B': 'black'}
+        self.feedback_colors = {'G': 'green', 'Y': 'yellow', 'B': 'dark grey'}
 
+        self.entries = []
         for i in range(5):
             validate_command = self.register(self.limit_characters)
-            guess_entry = tk.Entry(self, width=2, validate='key', validatecommand=(validate_command, '%P'))
-            guess_entry.bind('<Key>', lambda e, i=i: self.focus_next(i))
-            guess_entry.place(x=100 + i*50, y=100)  # adjust placement
-            self.guess_entry_fields.append(guess_entry)
+            entry = tk.Entry(self, width=2, validate='key', validatecommand=(validate_command, '%P'))
+            entry.config(font=('Helvetica', 24), justify='center', bg='dark grey')
+            entry.bind('<Key>', lambda e, i=i: self.focus_next(i))
+            entry.place(x=50 + i * 70, y=100)  # adjust placement
+            self.entries.append(entry)
 
+        self.feedback_buttons = []
         for i in range(5):
-            feedback_field = tk.Button(self, text='B', width=2)
-            feedback_field.place(x=100 + i*50, y=150)  # adjust placement
-            feedback_field.bind('<Button-1>', self.cycle_feedback)
-            self.guess_feedback_fields.append(feedback_field)
+            button = tk.Button(self, text='B', width=2, height=1, bg='dark grey')
+            button.config(font=('Helvetica', 24))
+            button.place(x=50 + i * 70, y=150)  # adjust placement
+            button.bind('<Button-1>', self.cycle_feedback)
+            self.feedback_buttons.append(button)
 
         submit_button = tk.Button(self, text='Submit', command=self.submit)
-        submit_button.place(x=175, y=200)  # centered placement
+        submit_button.place(x=150, y=220)  # centered placement
 
-        result_label = tk.Label(self, text='')
-        result_label.place(x=100, y=250)  # centered placement
-        self.result_label = result_label
+        self.result_label = tk.Label(self, text='', bg='light grey')
+        self.result_label.place(x=100, y=280)  # centered placement
 
-    def limit_characters(self, input):
+        self.previous_guesses_label = tk.Label(self, text='Previous Guesses:', bg='light grey')
+        self.previous_guesses_label.place(x=20, y=320)
+
+        self.previous_guesses_text = tk.Text(self, height=5, width=20)
+        self.previous_guesses_text.place(x=20, y=350)
+
+    @staticmethod
+    def limit_characters(input):
         return len(input) <= 1
 
     def focus_next(self, index):
         if index < 4:
-            self.guess_entry_fields[index+1].focus()
+            self.entries[index + 1].focus()
 
     def submit(self):
-        guessed_word = ''.join(entry.get().lower() for entry in self.guess_entry_fields)
-        feedback = [field['text'] for field in self.guess_feedback_fields]
+        guessed_word = ''.join(entry.get().lower() for entry in self.entries)
+        feedback = [button['text'] for button in self.feedback_buttons]
         self.solver.adjust_guess(guessed_word, feedback)
         optimal_guess = self.solver.get_optimal_guess()
         self.result_label['text'] = f'Optimal Guess: {optimal_guess}'
         self.guesses.append((guessed_word, feedback))
+
+        # reset entries and feedback buttons
+        for entry, button in zip(self.entries, self.feedback_buttons):
+            entry.delete(0, tk.END)
+            button['text'] = 'B'
+            button['bg'] = 'dark grey'
+
+        # update previous guesses
+        self.previous_guesses_text.insert(tk.END, f'{guessed_word} {feedback}\n')
 
     def cycle_feedback(self, event):
         current_feedback = event.widget['text']
@@ -55,6 +74,7 @@ class WordleGUI(tk.Tk):
         new_feedback = feedback_values[(feedback_values.index(current_feedback) + 1) % len(feedback_values)]
         event.widget['text'] = new_feedback
         event.widget['bg'] = self.feedback_colors[new_feedback]
+
 
 if __name__ == "__main__":
     solver = WordleSolver('validwords.txt', 'word_freq_map.json', 'letter_freq_map.json')
--- wordle_solver.py
+++ wordle_solver.py
@@ -1,55 +1,73 @@
-# wordle_solver.py
-
 import json
 
 class WordleSolver:
+    """
+    A class to encapsulate a Wordle game solver.
+    """
+
     def __init__(self, valid_words_file, word_freq_file, letter_freq_file):
-        self.words = self.load_words(valid_words_file)
-        self.word_freq = self.load_freq(word_freq_file)
-        self.letter_freq = self.load_freq(letter_freq_file)
+        # Load the valid words, word frequencies and letter frequencies at initialization
+        self.words = self._load_words(valid_words_file)
+        self.word_freq = self._load_freq(word_freq_file)
+        self.letter_freq = self._load_freq(letter_freq_file)
         self.possible_words = self.words.copy()
 
-    def load_words(self, file):
+    def _load_words(self, file):
+        """
+        Load words from a file. Assumes one word per line.
+        """
         with open(file, 'r') as f:
             words = f.read().splitlines()
         return words
 
-    def load_freq(self, file):
+    def _load_freq(self, file):
+        """
+        Load frequencies (word or letter) from a file. Assumes file is in JSON format.
+        """
         with open(file, 'r') as f:
             freq = json.load(f)
         return freq
 
     def adjust_guess(self, guessed_word, feedback):
-        new_possible_words = []
-        for word in self.possible_words:
-            if self.is_possible(word, guessed_word, feedback):
-                new_possible_words.append(word)
+        """
+        Given a word and feedback, adjust the list of possible words.
+        """
+        new_possible_words = [word for word in self.possible_words if self._is_possible(word, guessed_word, feedback)]
         self.possible_words = new_possible_words
 
-    def is_possible(self, word, guessed_word, feedback):
+    def _is_possible(self, word, guessed_word, feedback):
+        """
+        Check if a word is a possible solution given a guessed word and feedback.
+        """
         for i in range(5):
-            if feedback[i] == 'G' and word[i] != guessed_word[i]:
-                return False
-            if feedback[i] == 'B' and word[i] == guessed_word[i]:
-                return False
-            if feedback[i] == 'Y' and guessed_word[i] == word[i]:
-                return False
-            if feedback[i] == 'Y' and not word.__contains__(guessed_word[i]):
+            if ((feedback[i] == 'G' and word[i] != guessed_word[i]) or
+                (feedback[i] == 'B' and word[i] == guessed_word[i]) or
+                (feedback[i] == 'Y' and (guessed_word[i] == word[i] or not word.__contains__(guessed_word[i])))):
                 return False
         return True
 
     def get_optimal_guess(self):
-        if self.possible_words:
-            print(self.possible_words)
-            optimal_word = max(self.possible_words, key=self.get_word_score)
-            return optimal_word
-        else:
+        """
+        Get the most optimal guess from the list of possible words.
+        If there are no possible words left, return a message.
+        """
+        if not self.possible_words:
             return "No optimal guess could be made."
 
-    def get_word_score(self, word):
+        # Select the word with the highest score as the optimal guess
+        optimal_word = max(self.possible_words, key=self._get_word_score)
+        return optimal_word
+
+    def _get_word_score(self, word):
+        """
+        Calculate the score of a word, considering both the word frequency and letter frequencies.
+        """
         word_score = self.word_freq.get(word, 0)
         letter_score = sum(self.letter_freq.get(letter, 0) for letter in word)
         return word_score + letter_score
 
     def reset(self):
+        """
+        Reset the solver to the initial state.
+        """
         self.possible_words = self.words.copy()
