584cc2398d674795d546647d935223db52d52b1e
KanaiGooding <kgooding@hawaii.edu>
2023-10-28 21:48:07 -1000

    misc

--- WordleSolver/wordle_gui.py
+++ WordleSolver/wordle_gui.py
@@ -1,69 +1,113 @@
-# wordle_gui.py
+# wordle_solver_gui.py
 
 import tkinter as tk
 from wordle_solver import WordleSolver
 
 
-class WordleGUI(tk.Tk):
+class WordleSolverGUI(tk.Tk):
     def __init__(self, solver):
         super().__init__()
+
+        # Configure main window
         self.configure(bg='light grey')
         self.title('Wordle Solver')
         self.geometry('400x300')
+
+        # Initialize solver and guesses
         self.solver = solver
         self.guesses = []
+
+        # Define feedback color code
         self.feedback_colors = {'G': 'green', 'Y': 'yellow', 'B': 'dark grey'}
 
-        self.entries = []
+        # Initialize entry fields and feedback buttons
+        self.entry_fields = self._initialize_entries()
+        self.feedback_buttons = self._initialize_buttons()
+
+        # Initialize submit button
+        self._initialize_submit_button()
+
+        # Initialize result and possible guesses labels
+        self.result_label, self.possible_guesses_label = self._initialize_labels()
+
+    def _initialize_entries(self):
+        """Creates and places the entry fields in the window."""
+        entries = []
         for i in range(5):
-            validate_command = self.register(self.limit_characters)
-            entry = tk.Entry(self, width=2, validate='key', validatecommand=(validate_command, '%P'))
-            entry.config(font=('Helvetica', 24), justify='center', bg='dark grey')
-            entry.bind('<Key>', lambda e, i=i: self.focus_next(i))
-            entry.place(x=50 + i * 70, y=100)  # adjust placement
-            self.entries.append(entry)
+            entry = self._create_entry_field(i)
+            entries.append(entry)
+        return entries
 
-        self.feedback_buttons = []
+    def _create_entry_field(self, index):
+        """Creates a single entry field with the provided index."""
+        entry_position = 50 + index * 70
+        validate_command = self.register(self.limit_input_to_single_character)
+        entry = tk.Entry(self, width=2, validate='key', validatecommand=(validate_command, '%P'))
+        entry.config(font=('Helvetica', 24), justify='center', bg='dark grey')
+        entry.bind('<Key>', lambda e, i=index: self.focus_on_next_entry(i))
+        entry.place(x=entry_position, y=100)
+        return entry
+
+    def _initialize_buttons(self):
+        """Creates and places the feedback buttons in the window."""
+        buttons = []
         for i in range(5):
-            button = tk.Button(self, text='B', width=2, height=1, bg='dark grey')
-            button.config(font=('Helvetica', 24))
-            button.place(x=50 + i * 70, y=150)  # adjust placement
-            button.bind('<Button-1>', self.cycle_feedback)
-            self.feedback_buttons.append(button)
+            button = self._create_feedback_button(i)
+            buttons.append(button)
+        return buttons
 
-        submit_button = tk.Button(self, text='Submit', command=self.submit)
-        submit_button.place(x=150, y=220)  # centered placement
+    def _create_feedback_button(self, index):
+        """Creates a single feedback button with the provided index."""
+        button_position = 50 + index * 70
+        button = tk.Button(self, text='B', width=2, height=1, bg='dark grey')
+        button.config(font=('Helvetica', 24))
+        button.place(x=button_position, y=150)
+        button.bind('<Button-1>', self.cycle_feedback_color)
+        return button
 
-        self.result_label = tk.Label(self, text='', bg='light grey')
-        self.result_label.place(x=100, y=280)  # centered placement
+    def _initialize_submit_button(self):
+        """Creates and places the submit button in the window."""
+        submit_button = tk.Button(self, text='Submit', command=self.submit_guess)
+        submit_button.place(x=150, y=220)
 
-        self.possible_guesses_label = tk.Label(self, text='', bg='light grey')
-        self.possible_guesses_label.place(x=100, y=250)
+    def _initialize_labels(self):
+        """Creates and places the result and possible guesses labels in the window."""
+        result_label = tk.Label(self, text='', bg='light grey')
+        result_label.place(x=100, y=280)
 
-    def limit_characters(self, input):
+        possible_guesses_label = tk.Label(self, text='', bg='light grey')
+        possible_guesses_label.place(x=100, y=250)
+
+        return result_label, possible_guesses_label
+
+    def limit_input_to_single_character(self, input):
+        """Ensure the input in the entry fields is only a single character."""
         return len(input) <= 1
 
-    def focus_next(self, index):
+    def focus_on_next_entry(self, index):
+        """Focuses on the next entry field after a key press."""
         if index < 4:
-            self.entries[index + 1].focus()
+            self.entry_fields[index + 1].focus()
 
-    def submit(self):
-        guessed_word = ''.join(entry.get().lower() for entry in self.entries)
+    def submit_guess(self):
+        """Submits the current guess and provides feedback."""
+        guessed_word = ''.join(entry.get().lower() for entry in self.entry_fields)
         feedback = [button['text'] for button in self.feedback_buttons]
         self.solver.adjust_guess(guessed_word, feedback)
         optimal_guess = self.solver.get_optimal_guess()
         self.result_label['text'] = f'Optimal Guess: {optimal_guess}'
         self.guesses.append((guessed_word, feedback))
 
-        # reset entries and feedback buttons
-        for entry, button in zip(self.entries, self.feedback_buttons):
+        # Reset entry fields and feedback buttons
+        for entry, button in zip(self.entry_fields, self.feedback_buttons):
             entry.delete(0, tk.END)
             button['text'] = 'B'
             button['bg'] = 'dark grey'
 
         self.possible_guesses_label['text'] = f'Possible guesses remaining: {len(self.solver.possible_words)}'
 
-    def cycle_feedback(self, event):
+    def cycle_feedback_color(self, event):
+        """Cycles through feedback color options when the feedback button is clicked."""
         current_feedback = event.widget['text']
         feedback_values = list(self.feedback_colors.keys())
         new_feedback = feedback_values[(feedback_values.index(current_feedback) + 1) % len(feedback_values)]
@@ -72,7 +116,11 @@ class WordleGUI(tk.Tk):
 
 
 if __name__ == "__main__":
-    solver = WordleSolver('../frequencyMaps/validwords.txt', '../frequencyMaps/word_freq_map.json',
+    # Instantiate the solver
+    solver = WordleSolver('../frequencyMaps/validwords.txt',
+                          '../frequencyMaps/word_freq_map.json',
                           '../frequencyMaps/letter_freq_map.json')
-    gui = WordleGUI(solver)
-    gui.mainloop()
+
+    # Create GUI and start the main loop
+    solver_gui = WordleSolverGUI(solver)
+    solver_gui.mainloop()
--- WordleSolver/wordle_solver_tester.py
+++ WordleSolver/wordle_solver_tester.py
@@ -1,39 +1,38 @@
 import os
 import numpy
-from datetime import datetime
 from wordle_solver import WordleSolver
 from concurrent.futures import ProcessPoolExecutor
-import itertools
 
 
 class WordleSolverTester:
-
     def __init__(self, wordle_solver):
         self.solver = wordle_solver
         self.result_folder = 'testingResults'
         self.result_file = 'results.txt'
         self.stats_file = 'stats.txt'
         self.word_attempts = []
+        self.result_data = []
 
     def test_solver(self):
         os.makedirs(self.result_folder, exist_ok=True)
 
         # Create a ProcessPoolExecutor
-        with ProcessPoolExecutor() as executor:
+        with ProcessPoolExecutor(max_workers=10) as executor:
             # Use the map function to assign each word to a process in the pool
             results = executor.map(self.test_word, self.solver.words)
 
             # Iterate through the results as they become available
             for result in results:
                 self.word_attempts.append(result[1])
+                self.result_data.append(f'Word: {result[0]}, Attempts: {result[1]}, Guesses: {result[2]}\n')
 
-                with open(os.path.join(self.result_folder, self.result_file), 'a') as f:
-                    f.write(f'Word: {result[0]}, Attempts: {result[1]}, Guesses: {result[2]}\n')
+        # Write all results to the file in a single operation
+        with open(os.path.join(self.result_folder, self.result_file), 'w') as f:
+            f.writelines(self.result_data)
 
         self.generate_stats()
 
     def test_word(self, word):
-        self.solver.reset()
         attempts = 0
         guesses = []
 
@@ -47,18 +46,17 @@ class WordleSolverTester:
             else:
                 self.solver.adjust_guess(guess, feedback)
                 guess = self.solver.get_optimal_guess()  # next guesses will be optimal based on previous feedback
-        return word, attempts, guesses
 
     def generate_feedback(self, target, guess):
-        feedback = []
+        feedback = ''
         for t, g in zip(target, guess):
             if t == g:
-                feedback.append('G')
+                feedback += 'G'
             elif g in target:
-                feedback.append('Y')
+                feedback += 'Y'
             else:
-                feedback.append('B')
-        return ''.join(feedback)
+                feedback += 'B'
+        return feedback
 
     def generate_stats(self):
         min_attempts = min(self.word_attempts)
@@ -66,15 +64,16 @@ class WordleSolverTester:
         avg_attempts = numpy.mean(self.word_attempts)
         median_attempts = numpy.median(self.word_attempts)
         std_dev_attempts = numpy.std(self.word_attempts)
+        percent_success = len([r for r in self.word_attempts if r <= 6]) / 129.53
 
         with open(os.path.join(self.result_folder, self.stats_file), 'a') as f:
             f.write(f'Iteration: \n')
-            f.write(f'Time: {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}\n')
             f.write(f'Average attempts: {avg_attempts}\n')
             f.write(f'Minimum attempts: {min_attempts}\n')
             f.write(f'Maximum attempts: {max_attempts}\n')
             f.write(f'Median attempts: {median_attempts}\n')
             f.write(f'Standard Deviation of attempts: {std_dev_attempts}\n')
+            f.write(f'Percentage of words solved with less or equal to 6 attempts: {percent_success}\n')
             f.write(f'============================================================================\n')
 
 
